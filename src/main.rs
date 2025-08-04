43b4fcf580324595bfea8dd4ab5bfe7f89c4af71
MohamedAzizGharbi <gharbi.aziz03@gmail.com>
2025-08-04 18:39:58 +0200

    Second Commit
    Accepting Command Line Arguments
    Reading a file
    Seperating Logic and main() function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use minigrep::Config;
+use std::env;
+use std::process;
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments {err}");
+        process::exit(1);
+    });
+    println!("Searching for {}", config.query);
+    println!("In {}", config.file_path);
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
